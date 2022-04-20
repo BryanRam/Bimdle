03a1a5ba2b81d0b2d17c6e6aade077d182c0c543
Bradley <bryan_ram@hotmail.com>
2022-04-20 12:39:43 +0100

    Initial commit, wordle animations and colors, board created

---
+++ js/main.js
@@ -0,0 +1,113 @@
+document.addEventListener("DOMContentLoaded", () => {
+    createSquares();
+
+    let guessedWords = [[]];
+    let availableSpace = 1;
+    let word = "bajan";
+    let guessedWordCount = 0;
+
+    const keys = document.querySelectorAll(".keyboard-row button");
+
+
+    function getTileColor(letter, index){
+        const isCorrectLetter = word.includes(letter);
+
+        if (!isCorrectLetter){
+            return "rgb(58, 58, 60)";
+        }
+
+        const letterInThatPosition = word.charAt(index);
+        const isCorrectPosition = (letter === letterInThatPosition);
+
+        if (isCorrectPosition){
+            return "rgb(83, 141, 78)";
+
+        }
+        
+
+        return "rgb(181, 159, 59)";
+    }
+   
+
+    function handleSubmitWord(){
+        const currentWordArr = getCurrentWordArr()
+        
+        const firstLetterId = guessedWordCount * 5 + 1;
+        const interval = 200;
+        currentWordArr.forEach((letter, index) => {
+            setTimeout(() => {
+                const tileColor = getTileColor(letter, index);
+                const letterId = firstLetterId + index;
+                const letterEl = document.getElementById(letterId);
+                letterEl.classList.add("animate__flipInX");
+                letterEl.style = `background-color:${tileColor};border-color:${tileColor}`;
+            }, interval * index)
+        });
+
+        guessedWordCount += 1;
+
+        if(currentWordArr.length !== 5){
+            window.alert("Word must be 5 letters");
+        }
+
+        const currentWord = currentWordArr.join("")
+        console.log(currentWord);
+        console.log(word)
+
+        if (currentWord === word){
+            window.alert("Congratulations!")
+        }
+
+        if(guessedWords.length === 6){
+            window.alert(`Sorry you have no more guesses! The word is ${word}.`)
+        }
+
+        guessedWords.push([]);
+    }
+
+    function getCurrentWordArr(){
+        const numberOfGuessedWords = guessedWords.length;
+
+        return guessedWords[numberOfGuessedWords - 1]
+    }
+
+    function updateGuessedWords(letter){
+        const currentWordArr = getCurrentWordArr()
+
+        if (currentWordArr && currentWordArr.length < 5){
+            currentWordArr.push(letter);
+
+            const availableSpaceEl = document.getElementById(availableSpace)
+            availableSpace = availableSpace + 1
+
+            availableSpaceEl.textContent = letter;
+        }
+    }
+    
+    function createSquares(){
+        const gameBoard = document.getElementById("board")
+
+        for(let index=0; index < 30; index++){
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.classList.add("animate__animated");
+            square.setAttribute("id", index + 1);
+            gameBoard.appendChild(square);
+        }
+    }
+
+    for(let i = 0; i< keys.length; i++)
+    {
+        keys[i].onclick = ({ target }) => {
+            const letter = target.getAttribute("data-key");
+
+            if(letter === 'enter')
+            {
+                handleSubmitWord();
+                return;
+            }
+
+            updateGuessedWords(letter)
+        };
+    }
+})
