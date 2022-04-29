bdcaf6392d989546dc8abda590f2a7f1e8f1f051
Bradley <bryan_ram@hotmail.com>
2022-04-29 01:01:13 +0100

    Added charts.js dependency for guess stats

--- js/main.js
+++ js/main.js
@@ -35,6 +35,50 @@ $(document).ready(function(){
 
     const keys = document.querySelectorAll(".keyboard-row button");
 
+    const sCtx = document.getElementById('statsChart').getContext('2d');
+    
+    const labels = ["1","2","3","4","5","6"];
+    const data = {
+    labels: labels,
+    datasets: [{
+        axis: 'y',
+        label: '',
+        data: [65, 59, 80, 81, 56, 55],
+        backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)'
+        ],
+        borderColor: [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 205, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(54, 162, 235)',
+        'rgb(153, 102, 255)'
+        ],
+        borderWidth: 1
+    }]
+    };
+
+    const config = {
+        type: 'bar',
+        data: data,
+        options: {
+            indexAxis: 'y',
+            scales: {
+                x: {
+                    display: false
+                }
+            }
+        },
+      };
+
+    const statsChart = new Chart(sCtx, config);  
+
     shareButton.addEventListener('click', event => {
         if (navigator.share) {
           navigator.share({
@@ -122,13 +166,13 @@ $(document).ready(function(){
             if (currentWord === word){
                 guessed = true;
                 //window.alert("Congratulations!");
-                $('#exampleModal').modal('show'); 
+                $('#statisticsModal').modal('show'); 
             }
 
             if(guessedWords.length === 6 && currentWord !== word){
                 //window.alert(`Sorry you have no more guesses! The word is ${word}.`)
                 guessedWordCount = "X";
-                $('#exampleModal').modal('show');
+                $('#statisticsModal').modal('show');
             }
 
             guessedWords.push([]);
